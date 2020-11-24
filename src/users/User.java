e935a3df280a7bce472be0f5b9650dc8e66e0146
Nikolai Shilenko <nick.schilenko@gmail.com>
2020-11-24 17:30:02 +0400

    Add .gitignore

---
+++ src/users/User.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package users;
+
+/**
+ * @author Nikolai Shilenko
+ *
+ */
+public class User {
+
+	/**
+	 * 
+	 */
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
